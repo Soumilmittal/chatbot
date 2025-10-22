8b02ee16ffef5a9b2af80eee333d5bba01560193
Soumilmittal <mittasoumill@gmail.com>
2025-10-23 02:22:33 +0530

    new file:   .gitignore
    	new file:   README.md
    
    	new file:   eslint.config.js
    
    	new file:   index.html
    
    	new file:   package-lock.json
    
    	new file:   package.json
    
    	new file:   public/vite.svg
    
    	new file:   src/App.jsx
    
    	new file:   src/components/ChartbotIcon.jsx
    
    	new file:   src/index.css
    
    	new file:   src/main.jsx
    
    	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="chartbot-popup">
+      <div className="chart-header">
+        <div className="chart-info">
+          <h2 className='logo-text'>ChartBot</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ChartbotIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChartbotIcon = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChartbotIcon
