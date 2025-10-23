5c0fdf646c4f3b2a3d724a6b6978d44321d98447
Soumilmittal <mittasoumill@gmail.com>
2025-10-24 01:16:34 +0530

    added icon and links

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
+    <div className="container">
     <div className="chartbot-popup">
       <div className="chart-header">
         <div className="chart-info">
+          <ChartbotIcon />
           <h2 className='logo-text'>ChartBot</h2>
         </div>
       </div>
     </div>
+  </div>
   )
 }
 
