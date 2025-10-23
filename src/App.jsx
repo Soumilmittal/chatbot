cea4494aa65824533f40f965fe4c68e7771518fb
Soumilmittal <mittasoumill@gmail.com>
2025-10-24 03:03:37 +0530

    added index and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
+import ChartbotIcon from './components/ChartbotIcon';
+
 
 const App = () => {
   return (
     <div className="container">
-    <div className="chartbot-popup">
-      <div className="chart-header">
-        <div className="chart-info">
-          <ChartbotIcon />
-          <h2 className='logo-text'>ChartBot</h2>
+      <div className="chartbot-popup">
+        {/* Chatbot Header*/}
+        <div className="chart-header">
+          <div className="chart-info">
+            <ChartbotIcon />
+            <h2 className='logo-text'>ChartBot</h2>
+          </div>
+          <button className="material-symbols-rounded">keyboard_arrow_down</button>
+        </div>
+
+        {/* Chatbot body*/}
+        <div className="chartbot-body">
+          <div className="message bot-message">
+            <ChartbotIcon />
+            <p className='message-text'>
+              Hey there <br /> How can i help you today?
+            </p>
+          </div>
+          <div className="message user-message">
+            <p className='message-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi consequatur sit? Impedit ratione nesciunt quo tempora quae placeat obcaecati commodi, tenetur animi soluta sint sunt, culpa possimus eum est.
+            </p>
+          </div>
+        </div>
+
+        {/* Chatbot Footer*/}
+        <div className="chart-footer">
+          <form action="" className="chat-form">
+          <input type="text" placeholder='Type a message...'
+          className='message-input' required/>
+          <button className="material-symbols-rounded">arrow_upward</button>
+          </form>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
 
-export default App
+  );
+};
+
+export default App;
